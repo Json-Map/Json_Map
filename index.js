e910e78ac9c86c32fe5111384d05409ca81e1674
GHOSTON <achraf.elbihi@gmail.com>
2024-05-06 23:30:25 +0100

    2024-05-06T23:30:25+01:00

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const makeCommit = n => {
   const x = Math.floor(Math.random() * 55);
   const y = Math.floor(Math.random() * 7);
   
-  const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+  const DATE = moment().subtract(2, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
   
   const data = {
     date: DATE
